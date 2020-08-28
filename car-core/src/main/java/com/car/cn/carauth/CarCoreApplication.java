7f53beb1fcbe035c34e8e697b8479a7ec326a59e
bigbao1993 <1191269844@qq.com>
2020-08-28 20:13:59 +0800

    Initial commit

---
+++ car-core/src/main/java/com/car/cn/carauth/CarCoreApplication.java
@@ -0,0 +1,13 @@
+package com.car.cn.carauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarCoreApplication.class, args);
+    }
+
+}
