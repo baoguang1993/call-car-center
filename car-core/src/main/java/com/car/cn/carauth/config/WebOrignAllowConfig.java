f2dc7ae06793bd6362230c896dcd57a7e8d54837
bigbao1993 <1191269844@qq.com>
2020-09-30 18:12:07 +0800

    基础配置

---
+++ car-core/src/main/java/com/car/cn/carauth/config/WebOrignAllowConfig.java
@@ -0,0 +1,23 @@
+package com.car.cn.carauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class WebOrignAllowConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**" , corsConfiguration);
+        return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+}
